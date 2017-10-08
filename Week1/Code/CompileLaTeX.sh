a04a0fa574168d649161f74917ac55cf6bd3fce9
Marie Russell <m.russell17@imperial.ac.uk>
2017-10-08 16:54:20 +0100

    file that compiles LaTeX was modified

--- Week1/Code/CompileLaTeX.sh
+++ Week1/Code/CompileLaTeX.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-pdflatex $1.tex
-pdflatex $1.tex
+pdflatex $1
+pdflatex $1
 bibtex $1
-pdflatex $1.tex
-pdflatex $1.tex
-evince $1.pdf &
+pdflatex $1
+pdflatex $1
+evince $1 &
 
 ## Cleanup
 
